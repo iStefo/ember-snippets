1cf5cc60219a6e35ef50e2b2fd0d39fa848997c8
Stefan Fochler <stefan@fochler.de>
2013-08-27 10:03:19 +0200

    update ember-atomic-radio

--- ember-atomic-radio/README.md
+++ ember-atomic-radio/README.md
@@ -3,8 +3,10 @@ This is a Radio Button that behaves like a radio button should within its name w
 {{ember-atomic-radio name="my-group"}}
 ```
 
-What makes it atomic is, that you don't have to bind the 'value' property of the radio button group, but you can bind the 'selected' property to your model:
+What makes it atomic is, that you don't have to bind the 'value' property of the radio button group, but you can bind the 'checked' property to your model:
 ```hbs
-{{ember-atomic-radio name="my-group" selected=wizzard.isChosenOne}}
+{{ember-atomic-radio name="my-group" checked=wizzard.isChosenOne}}
 ```
 when used e.g. inside an #each loop.
+
+**Note**: This component does not maintain the radio group invariant if you set the 'checked' property manually (so you can end up with more then one object "checked" if you don't do it by clicking)
--- ember-atomic-radio/ember_atomic_radio.js
+++ ember-atomic-radio/ember_atomic_radio.js
@@ -3,13 +3,16 @@
  * {{ember-atomic-radio name="my-group"}}
  *
  * What makes it atomic is, that you don't have to bind the 'value' property of the radio button group
- * but you can bind the 'selected' property to your model
- * {{ember-atomic-radio name="my-group" selected=wizzard.isChosenOne}}
+ * but you can bind the 'checked' property to your model
+ * {{ember-atomic-radio name="my-group" checked=wizzard.isChosenOne}}
  * when used e.g. inside an #each loop
+ *
+ * Note: This component does not maintain the radio group invariant if you set the 'checked' property manually
+ * (so you can end up with more then one object "checked" if you don't do it by clicking)
  */
 var EmberAtomicRadio = Ember.Component.extend({
   tagName: 'input',
-  attributeBindings: ['type', 'name', 'value'],
+  attributeBindings: ['type', 'name', 'value', 'checked'],
   type: 'radio',
   // name and value should be set via templates
 
@@ -24,10 +27,14 @@ var EmberAtomicRadio = Ember.Component.extend({
     $('body').off('change', this.changeListener);
   },
 
+  /**
+   * Changing the checked value from the outside will change the state of the input element
+   * In the other direction, we have to listen for a change and update the 'checked' property
+   */
   changed: function() {
     // any of the radio inputs for this group changed.
     // test own element and save its state
-    this.set('selected', this.$().is(':checked'));
+    this.set('checked', this.$()[0].checked);
   }
 });
 
